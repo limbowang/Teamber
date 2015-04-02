953cc0e02a9799359f10fd299c584d9a9b3348cf
limbowang <van8701@gmail.com>
2015-04-02 23:10:57 +0800

    Init project with express.

---
+++ db/teamber.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50621
+Source Host           : localhost:3306
+Source Database       : teamber
+
+Target Server Type    : MYSQL
+Target Server Version : 50621
+File Encoding         : 65001
+
+Date: 2015-04-02 23:09:08
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
