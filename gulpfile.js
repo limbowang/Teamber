5f0532b63ed1e10ec7d12a948214b7ea63618873
limbowang <van8701@gmail.com>
2015-04-05 02:37:00 +0800

    Add sequelize as orm and karma and jasmine as unit test framework. Add gulp support..

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var Sequelize = require("sequelize");
+
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('migrate', function() {
+	var env       = process.env.NODE_ENV || "development";
+	var config    = require(__dirname + '/config/database.json')[env];
+	var sequelize = new Sequelize(config.database, config.username, config.password, config);
+	var migrator = sequelize.getMigrator({
+		path: __dirname + '/migrations',
+	});
+	
+	migrator
+		.migrate({method: 'up'})
+		.success(function() {
+			console.log("Migrate successfully.");
+			process.exit(0);
+		})
+});
